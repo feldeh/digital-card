cdad4fa05a928439cffb18a24021c947c551485e
fel <felicien.dehertogh@protonmail.com>
2022-10-31 01:09:20 +0100

    populate profile page bio on page load

---
+++ js/index.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', updateBio);
+
+let fullName, jobTitle, companyName, OneLine;
+
+fullName = document.querySelector('#fullName');
+jobTitle = document.querySelector('#jobTitle');
+companyName = document.querySelector('#companyName');
+oneLine = document.querySelector('#oneLine');
+
+function updateBio() {
+  let jsonForm = window.localStorage.getItem('form');
+  if (jsonForm) {
+    const form = JSON.parse(jsonForm);
+    fullName.textContent = form.fullName;
+    jobTitle.textContent = form.jobTitle;
+    companyName.textContent = form.companyName;
+    oneLine.textContent = form.oneLine;
+  } else {
+    fullName.textContent = 'John Nolan';
+    jobTitle.textContent = 'Back-end developer';
+    companyName.textContent = 'Listri Digital';
+    oneLine.textContent =
+      'Experience is the name everyone gives to their mistakes.';
+  }
+}
